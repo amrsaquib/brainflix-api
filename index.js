75cd40decdf3185659e3b5588085ad21fe8c51fb
amrsaquib <amrsaquib@gmail.com>
2023-07-06 17:00:31 -0400

    finished the static files part

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const uuid = require('uuid')
 app.use(express.json())
+app.use(express.static("public"))
 
 app.get('/videos', (_req, res) => {
     let returnVideos = videos.map(video => {
